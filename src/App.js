d3cc45845c0a0086e4146587d83bffb233a1c930
Clover-Codes <95234232+Clover-Codes@users.noreply.github.com>
2022-02-03 12:34:38 +0530

    base game copy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello World</p>
+      <p>lorem, huh so if i do anything it should reflext back to the output. right</p>
+      {/* <div style={{height: "100px", width: "200px", background: "red"}}></div> */}
     </div>
   );
 }
